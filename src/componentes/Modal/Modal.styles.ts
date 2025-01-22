fc46ede3d808d7cf7944f5084f584a0dd6862f0b
Karine <karinedeveloper@gmail.com>
2025-01-22 00:14:52 -0300

    feat: add component Modal

---
+++ src/componentes/Modal/Modal.styles.ts
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+export const customStylesModal = {
+  content: {
+    width: '254px',
+    top: 0,
+    left: 'auto',
+    right: 0,
+    bottom: 0,
+    height: '100%',
+    marginRight: 0,
+    transform: 'none',
+    backgroundColor: 'rgba(253, 253, 253, 0.99)',
+    border: 'none',
+    overflowY: 'auto' as const,
+    padding: '20px',
+    backdropFilter: 'blur(10px)',
+    WebkitBackdropFilter: 'blur(10px)'
+  },
+  overlay: {
+    backgroundColor: 'rgba(0, 0, 0, 0.4)'
+  }
+};
+
+export const Close__Modal = styled.div`
+  display: flex;
+  justify-content: flex-end;
+
+  button {
+    color: black;
+  }
+`;
+
+export const Open__Modal = styled.button`
+  color: ${({ theme }) => theme.colors.neutral.medium};
+  font-size: ${({ theme }) => theme.fonts.size.md};
+  padding-right: 64px;
+`;
+
+export const Wrapper__Modal = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 34px;
+  justify-content: flex-start;
+`;
