cd7822d786c5d288f3cbea0800c02dd215c2e63e
Kelvin Charles <kelvinc1291@hotmail.com>
2025-01-26 23:37:28 -0300

    feat: add container hero

---
+++ src/containers/Hero/index.ts
@@ -0,0 +1,7 @@
+import { Badge } from './Badge';
+import { Title } from './Title';
+import { Wrapper } from './Wrapper';
+
+const HeroElements = { HeroTitle: Title, Wrapper, Badge };
+
+export default HeroElements;
